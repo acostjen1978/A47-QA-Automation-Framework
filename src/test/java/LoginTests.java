8197c5b014ab5d9739f8b9a4cefab346dc4d08ee
acostjen1978 <131210089+acostjen1978@users.noreply.github.com>
2023-06-27 17:58:23 -0400

    Jenniffer Acosta - Homework-16
    Registration Page - test passed

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -8,7 +10,7 @@ import java.time.Duration;
 
 public class LoginTests extends BaseTest {
     @Test
-    public void LoginEmptyEmailPasswordTest() {
+    public void  registrationNavigation() {
 
 //      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
@@ -17,9 +19,17 @@ public class LoginTests extends BaseTest {
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://qa.koel.app/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+         String url = "https://qa.koel.app/";
+         driver.get(url);
+
+         WebElement registrationInput = driver.findElement(By.cssSelector("[id='hel']"));
+         registrationInput.click();
+
+         WebElement RegisterNewAccount = driver.findElement(By.cssSelector("div[class='login-wrapper'] h2"));
+         Assert.assertTrue(RegisterNewAccount.isDisplayed());
+
+         driver.quit();
+
+
     }
 }
