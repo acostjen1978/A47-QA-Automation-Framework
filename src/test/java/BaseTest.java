7d6f92bc2e5b6394ce55f16dc60c52ef5001bc79
acostjen1978 <131210089+acostjen1978@users.noreply.github.com>
2023-07-14 19:31:22 -0400

    Homework21 Jenniffer
    Actions

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,216 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.BeforeSuite;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+import java.util.UUID;
 
 public class BaseTest {
+
+    public static WebDriver driver = null;
+    public static WebDriverWait wait = null;
+
+    public static Actions actions = null;
+    public static String url = "https://qa.koel.app/";
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
+    @BeforeMethod
+    @Parameters({ "BaseURL" })
+    public void launchBrowser(String BaseURL) {
+        // Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+        url = BaseURL;
+        driver.get(url);
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        actions = new Actions(driver);
+
+    }
+
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+    @DataProvider(name = "IncorrectLoginProviders")
+    public static Object[][] getDataFromDataProviders() {
+        return new Object[][] {
+                { "notExisting@email.com", "NotExistingPassword" },
+                { "dulcelaergica@yahoo.com", "" },
+                { "", "" }
+        };
+    }
+
+    @DataProvider(name = "CorrectLoginProviders")
+    public static Object[][] getLoginData() {
+        return new Object[][] {
+                { "dulcealergica@yahoo.com", "te$t$tudent" }
+        };
+    }
+
+    protected static void clickSubmit() {
+        WebElement submitLogin = wait
+                .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[type='submit']")));
+        submitLogin.click();
+    }
+
+    protected static void enterPassword(String password) {
+        WebElement passwordInput = wait
+                .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='password']")));
+        passwordInput.click();
+        passwordInput.clear();
+        passwordInput.sendKeys(password);
+    }
+
+    protected static void enterEmail(String email) {
+        WebElement emailInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='email']")));
+        emailInput.click();
+        emailInput.clear();
+        emailInput.sendKeys(email);
+    }
+
+    protected static void openLoginUrl() {
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+    }
+
+    // Profile Tests Helper Functions
+    protected static void clickAvatarIcon() {
+        WebElement avatarIcon = driver.findElement(By.cssSelector("img.avatar"));
+        avatarIcon.click();
+    }
+
+    protected static String generateRandomName() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+    protected static void providePassword(String password) {
+        WebElement currentPassword = driver.findElement(By.cssSelector("[name='current_password']"));
+        currentPassword.clear();
+        currentPassword.sendKeys(password);
+    }
+    protected static void provideProfileName(String name) {
+        WebElement profileName = driver.findElement(By.cssSelector("[name='name']"));
+        profileName.clear();
+        profileName.sendKeys(name);
+    }
+    protected static void ClickSaveButton() {
+        WebElement saveButton = driver.findElement(By.cssSelector(("button.btn-submit")));
+        saveButton.click();
+    }
+    public void searchSong(String songTitleKeyword) throws InterruptedException {
+        WebElement searchField = driver.findElement(By.cssSelector(("div#searchForm input[type=search]")));
+        searchField.clear();
+        searchField.sendKeys(songTitleKeyword);
+        Thread.sleep(5000);
+    }
+    public void clickViewAllBtn() throws InterruptedException {
+        WebElement viewAllSearchResult = driver
+                .findElement(By.cssSelector("#searchExcerptsWrapper > div > div > section.songs > h1 > button"));
+        viewAllSearchResult.click();
+        Thread.sleep(5000);
+    }
+    public void selectFirstSongResult() throws InterruptedException {
+        WebElement firstSongResult = driver
+                .findElement(By.cssSelector("#songResultsWrapper > div > div > div.item-container > table > tr"));
+        firstSongResult.click();
+        Thread.sleep(5000);
+    }
+    public void clickAddToBtn() throws InterruptedException {
+        WebElement addToBtn = driver.findElement(
+                By.cssSelector("#songResultsWrapper > header > div.song-list-controls > span > button.btn-add-to"));
+        addToBtn.click();
+        Thread.sleep(5000);
+    }
+    public void choosePlayList() throws InterruptedException {
+        WebElement playListElement = driver
+                .findElement(By.xpath("//*[@id='songResultsWrapper']/header/div[3]/div/section[1]/ul/li[74]"));
+        playListElement.click();
+    }
+    public String getNotificationText() {
+        WebElement notificationElement = driver.findElement(By.cssSelector("div.alertify-logs.top.right"));
+        return notificationElement.getText();
+    }
+    public void clickPlay() {
+        WebElement playNextButton = wait
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='mainFooter']/div[1]/i[2]")));
+        WebElement playButton = wait
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='mainFooter']/div[1]/span/span[2]")));
+
+        playNextButton.click();
+        playButton.click();
+
+    }
+    public Boolean isSongPlaying() {
+        WebElement soundBar = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.xpath("//*[@id='mainFooter']/div[2]/div[2]/div/button[1]/div")));
+        return soundBar.isDisplayed();
+    }
+
+    public void openPlaylist() {
+        WebElement playlist = wait
+                .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
+        playlist.click();
+    }
+
+    public void clickDeletePlaylistBtn() {
+        WebElement deletePlaylist = wait
+                .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".btn-delete-playlist")));
+        deletePlaylist.click();
+    }
+
+    public void confirmDelete() {
+        WebElement confirmBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button.ok")));
+        confirmBtn.click();
+    }
+
+    public String getDeletedPlaylistMsg() {
+        WebElement notificationMsg = wait
+                .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
+        return notificationMsg.getText();
+    }
+    String newPlaylistName = "new name";
+    public void doubleClickPlaylist() {
+        WebElement doubleClickPlaylist = wait
+                .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlists:nth-child(3)")));
+         doubleClickPlaylist.click();
+         actions.doubleClick(driver.findElement(By.cssSelector(".playlists:nth-child(3)"))).perform();
+    }
+    // Helper functions for Rename
+
+    public void enterNewPlaylistName() {
+        WebElement playlistInputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='name']")));
+        playlistInputField.sendKeys(Keys.chord(Keys.CONTROL, "A", Keys.BACK_SPACE));
+
+        playlistInputField.sendKeys(newPlaylistName);
+        playlistInputField.sendKeys(Keys.ENTER);
+    }
+
+    public boolean doesPlaylistExist() {
+        WebElement playlistElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='" + newPlaylistName + "' ]")));
+        return playlistElement.isDisplayed();
+    }
+
+
 }
+
+
+
+
+
---
+++ src/test/java/Homework21.java
@@ -0,0 +1,60 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class Homework21 extends BaseTest {
+
+    @Test (dataProvider = "CorrectLoginProviders", dataProviderClass = BaseTest.class)
+    public void playSongTest(String email, String password) throws InterruptedException {
+
+
+        enterEmail(email);
+
+        enterPassword(password);
+
+        clickSubmit();
+
+        clickPlay();
+        Assert.assertTrue(isSongPlaying());
+    }
+
+    @Test (dataProvider = "CorrectLoginProviders", dataProviderClass = BaseTest.class)
+    public void deletePlaylist(String email, String password) throws InterruptedException{
+        String deletedPlaylistMsg="Deleted playlist";
+
+        //Put the email field inside the web page
+        enterEmail(email);
+        // Put the password inside the web app
+        enterPassword(password);
+        //Click on the submit button
+        clickSubmit();
+
+        openPlaylist();
+        clickDeletePlaylistBtn(); //optional
+        confirmDelete();
+
+        Assert.assertTrue(getDeletedPlaylistMsg().contains(deletedPlaylistMsg));
+        }
+
+    @Test(dataProvider = "CorrectLoginProviders", dataProviderClass = BaseTest.class)
+
+    public void renamePlaylist(String email, String password) throws InterruptedException {
+        String updatedPlaylistMsg = "Update playlist";
+
+        enterEmail(email);
+
+        enterPassword(password);
+
+        clickSubmit();
+
+        doubleClickPlaylist();
+        enterNewPlaylistName();
+
+        Assert.assertTrue(doesPlaylistExist());
+
+    }
+}
+
+
+
+
+
+
